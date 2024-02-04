4dd9293200764d768d0d37c0275834ec8da40065
Fabio Elie <fabioelie@outlook.com>
2024-02-04 15:22:16 -0400

    Alterando os Responses da Classe MaterialController

--- src/main/java/br/com/logistics/api/controller/MaterialController.java
+++ src/main/java/br/com/logistics/api/controller/MaterialController.java
@@ -10,8 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("materiais")
@@ -22,27 +24,32 @@ public class MaterialController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody @Valid MaterialDTO dados) {
-        repository.save(new Material(dados));
+    public ResponseEntity cadastrar(@RequestBody @Valid MaterialDTO dados, UriComponentsBuilder uriComponentsBuilder) {
+        var material = repository.save(new Material(dados));
+        var uri = uriComponentsBuilder.path("/materiais/{id}").buildAndExpand(material).toUri();
+        return  ResponseEntity.created(uri).body(material);
     }
 
     @GetMapping
-    public Page<DadosListagemMateriais> listar(@PageableDefault(size = 10, sort = {"nome"})Pageable paginacao) {
-        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMateriais::new);
+    public ResponseEntity<Page<DadosListagemMateriais>> listar(@PageableDefault(size = 10, sort = {"nome"})Pageable paginacao) {
+        var page = repository.findAllByAtivoTrue(paginacao).map(DadosListagemMateriais::new);
+        return ResponseEntity.ok(page);
     }
 
     @PutMapping
     @Transactional
-    public void atualizar(@RequestBody @Valid DadosAtualizacaoMaterial dados){
+    public ResponseEntity<DadosListagemMateriais> atualizar(@RequestBody @Valid DadosAtualizacaoMaterial dados){
         var material = repository.getReferenceById(dados.id());
         material.atualizarInformacoesDoMaterial(dados);
+        return ResponseEntity.ok(new DadosListagemMateriais(material));
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void excluir(@PathVariable Long id){
+    public ResponseEntity excluir(@PathVariable Long id){
         var material = repository.getReferenceById(id);
         material.excluir();
+        return ResponseEntity.noContent().build();
     }
 
 
